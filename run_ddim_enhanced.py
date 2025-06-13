002bc27ca08cd0b46d88de4b48b4336c7c0a4654
Mohamad Chaalan <mohamad@molecularinstruments.com>
2025-06-13 14:47:15 -0700

    Uploading enhanced_unet code

---
+++ run_ddim_enhanced.py
@@ -0,0 +1,314 @@
+import os
+
+import tensorflow as tf
+
+from dynamic_metrics import DynamicKID
+from eval import KID
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import time
+import matplotlib.pyplot as plt
+
+import keras
+from keras import layers
+
+from enhanced_unet import revolutionary_unet_model
+from ddim_utils import load_ddim_dataset, ModelType, ops, save_image, preprocess_img
+
+train = False
+normal = False
+
+# data & weights
+if normal:
+    dataset_path = "/Users/mohamadc/MI Dropbox Dropbox/Mohamad Chaalan/Mac/Downloads/chest_xray/train/NORMAL"
+    network_weights = "ddim_enhanced_unet/normal_enhanced_network_ddim.weights.h5"
+    ema_weights = "ddim_enhanced_unet/normal_enhanced_ema_ddim.weights.h5"
+else:
+    dataset_path = "/Users/mohamadc/MI Dropbox Dropbox/Mohamad Chaalan/Mac/Downloads/chest_xray/train/PNEUMONIA"
+    network_weights = "ddim_enhanced_unet/pneumonia_enhanced_network_ddim.weights.h5"
+    ema_weights = "ddim_enhanced_unet/pneumonia_enhanced_ema_ddim.weights.h5"
+
+dataset_repetitions = 5
+num_epochs = 301  # train for at least 50 epochs for good results
+image_size = 64
+# KID = Kernel Inception Distance, see related section
+kid_diffusion_steps = 5
+plot_diffusion_steps = 20
+
+# sampling
+min_signal_rate = 0.02
+max_signal_rate = 0.95
+
+# architecture
+widths = [32, 64, 96, 128]
+block_depth = 2
+
+# optimization
+batch_size = 64
+ema = 0.999
+learning_rate = 1e-3
+weight_decay = 1e-4
+
+# load dataset
+train_dataset = load_ddim_dataset(dataset_path, batch_size)
+
+@keras.saving.register_keras_serializable()
+class DiffusionModel(keras.Model):
+    def __init__(self, image_size, widths, block_depth):
+        super().__init__()
+
+        self.normalizer = layers.Normalization()
+        self.network = revolutionary_unet_model(image_size, widths, block_depth)
+        self.ema_network = keras.models.clone_model(self.network)
+
+    def compile(self, **kwargs):
+        super().compile(**kwargs)
+
+        self.noise_loss_tracker = keras.metrics.Mean(name="n_loss")
+        self.image_loss_tracker = keras.metrics.Mean(name="i_loss")
+        self.kid = KID(name="kid", img_size=image_size)
+
+    @property
+    def metrics(self):
+        return [self.noise_loss_tracker, self.image_loss_tracker, self.kid]
+
+    def denormalize(self, images):
+        # convert the pixel values back to 0-1 range
+        images = self.normalizer.mean + images * self.normalizer.variance**0.5
+        return ops.clip(images, 0.0, 1.0)
+
+    def diffusion_schedule(self, diffusion_times):
+        # diffusion times -> angles
+        start_angle = ops.cast(ops.arccos(max_signal_rate), "float32")
+        end_angle = ops.cast(ops.arccos(min_signal_rate), "float32")
+
+        diffusion_angles = start_angle + diffusion_times * (end_angle - start_angle)
+
+        # angles -> signal and noise rates
+        signal_rates = ops.cos(diffusion_angles)
+        noise_rates = ops.sin(diffusion_angles)
+        # note that their squared sum is always: sin^2(x) + cos^2(x) = 1
+
+        return noise_rates, signal_rates
+
+    def denoise(self, noisy_images, noise_rates, signal_rates, training):
+        # the exponential moving average weights are used at evaluation
+        if training:
+            network = self.network
+        else:
+            network = self.ema_network
+
+        # predict noise component and calculate the image component using it
+        pred_noises = network([noisy_images, noise_rates**2], training=training)
+        pred_images = (noisy_images - noise_rates * pred_noises) / signal_rates
+
+        return pred_noises, pred_images
+
+    def reverse_diffusion(self, initial_noise, diffusion_steps):
+        # reverse diffusion = sampling
+        num_images = initial_noise.shape[0]
+        step_size = 1.0 / diffusion_steps
+
+        # important line:
+        # at the first sampling step, the "noisy image" is pure noise
+        # but its signal rate is assumed to be nonzero (min_signal_rate)
+        next_noisy_images = initial_noise
+        for step in range(diffusion_steps):
+            noisy_images = next_noisy_images
+
+            # separate the current noisy image to its components
+            diffusion_times = ops.ones((num_images, 1, 1, 1)) - step * step_size
+            noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+            pred_noises, pred_images = self.denoise(
+                noisy_images, noise_rates, signal_rates, training=False
+            )
+            # network used in eval mode
+
+            # remix the predicted components using the next signal and noise rates
+            next_diffusion_times = diffusion_times - step_size
+            next_noise_rates, next_signal_rates = self.diffusion_schedule(
+                next_diffusion_times
+            )
+            next_noisy_images = (
+                next_signal_rates * pred_images + next_noise_rates * pred_noises
+            )
+            # this new noisy image will be used in the next step
+
+        return pred_images
+
+    def generate(self, num_images, diffusion_steps):
+        print("Generating images")
+        start_time = time.perf_counter()
+
+        # noise -> images -> denormalized images
+        initial_noise = keras.random.normal(
+            shape=(num_images, image_size, image_size, 3)
+        )
+        generated_images = self.reverse_diffusion(initial_noise, diffusion_steps)
+        generated_images = self.denormalize(generated_images)
+
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f"Elapsed time: {elapsed_time:.4f} seconds")
+
+        return generated_images
+
+    def train_step(self, images):
+        # normalize images to have standard deviation of 1, like the noises
+        images = self.normalizer(images, training=True)
+        noises = keras.random.normal(shape=(batch_size, image_size, image_size, 3))
+
+        # sample uniform random diffusion times
+        diffusion_times = keras.random.uniform(
+            shape=(batch_size, 1, 1, 1), minval=0.0, maxval=1.0
+        )
+        noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+        # mix the images with noises accordingly
+        noisy_images = signal_rates * images + noise_rates * noises
+
+        with tf.GradientTape() as tape:
+            # train the network to separate noisy images to their components
+            pred_noises, pred_images = self.denoise(
+                noisy_images, noise_rates, signal_rates, training=True
+            )
+
+            noise_loss = self.loss(noises, pred_noises)  # used for training
+            image_loss = self.loss(images, pred_images)  # only used as metric
+
+        gradients = tape.gradient(noise_loss, self.network.trainable_weights)
+        self.optimizer.apply_gradients(zip(gradients, self.network.trainable_weights))
+
+        self.noise_loss_tracker.update_state(noise_loss)
+        self.image_loss_tracker.update_state(image_loss)
+
+        # track the exponential moving averages of weights
+        for weight, ema_weight in zip(self.network.weights, self.ema_network.weights):
+            ema_weight.assign(ema * ema_weight + (1 - ema) * weight)
+
+        # KID is not measured during the training phase for computational efficiency
+        return {m.name: m.result() for m in self.metrics[:-1]}
+
+    def test_step(self, images):
+        # normalize images to have standard deviation of 1, like the noises
+        images = self.normalizer(images, training=False)
+        noises = keras.random.normal(shape=(batch_size, image_size, image_size, 3))
+
+        # sample uniform random diffusion times
+        diffusion_times = keras.random.uniform(
+            shape=(batch_size, 1, 1, 1), minval=0.0, maxval=1.0
+        )
+        noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+        # mix the images with noises accordingly
+        noisy_images = signal_rates * images + noise_rates * noises
+
+        # use the network to separate noisy images to their components
+        pred_noises, pred_images = self.denoise(
+            noisy_images, noise_rates, signal_rates, training=False
+        )
+
+        noise_loss = self.loss(noises, pred_noises)
+        image_loss = self.loss(images, pred_images)
+
+        self.image_loss_tracker.update_state(image_loss)
+        self.noise_loss_tracker.update_state(noise_loss)
+
+        # measure KID between real and generated images
+        # this is computationally demanding, kid_diffusion_steps has to be small
+        images = self.denormalize(images)
+        generated_images = self.generate(
+            num_images=batch_size, diffusion_steps=kid_diffusion_steps
+        )
+        self.kid.update_state(images, generated_images)
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def plot_images(self, epoch=None, logs=None, num_rows=3, num_cols=6):
+        if epoch % 50 == 0:
+            # plot random generated images for visual evaluation of generation quality
+            generated_images = self.generate(
+                num_images=num_rows * num_cols,
+                diffusion_steps=plot_diffusion_steps,
+            )
+
+            plt.figure(figsize=(num_cols * 2.0, num_rows * 2.0))
+            for row in range(num_rows):
+                for col in range(num_cols):
+                    index = row * num_cols + col
+                    plt.subplot(num_rows, num_cols, index + 1)
+                    plt.imshow(generated_images[index])
+                    plt.axis("off")
+            plt.tight_layout()
+            plt.show()
+            plt.close()
+
+    def download_images(self, epoch=None, logs=None, description="img_enhanced_normal", num_rows=3, num_cols=6):
+        generated_images = self.generate(
+            num_images=num_rows * num_cols,
+            diffusion_steps=plot_diffusion_steps,
+        )
+
+        for row in range(num_rows):
+            for col in range(num_cols):
+                index = row * num_cols + col
+                save_image(generated_images[index], f"{description}/image_{col}_{row}_{index}.png")
+
+# create and compile the model
+model = DiffusionModel(image_size, widths, block_depth)
+
+print("compiling model")
+model.compile(
+    optimizer=keras.optimizers.AdamW(
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        clipnorm=1.0  # Gradient clipping by global norm
+    ),
+    loss=tf.keras.losses.Huber(delta=1.0)
+)
+
+# calculate mean and variance of training dataset for normalization
+model.normalizer.adapt(train_dataset)
+
+if train:
+    print(model.summary())
+    print(model.network.summary())
+
+    # run training and plot generated images periodically
+    print("training model")
+    model.fit(
+        train_dataset,
+        epochs=num_epochs,
+        batch_size=batch_size,
+        callbacks=[
+            keras.callbacks.LambdaCallback(on_epoch_end=model.plot_images)
+        ],
+    )
+
+    print("save model")
+    model.network.save_weights(network_weights)
+    model.ema_network.save_weights(ema_weights)
+
+else:
+    print("load_model")
+    model.network.load_weights(network_weights)
+    model.ema_network.load_weights(ema_weights)
+
+    # model.download_images(description="img_enhanced_pneumonia", num_rows=100, num_cols=10)
+
+    # print("plotting images")
+    # model.plot_images(epoch=0, num_rows=1, num_cols=2)
+    #
+    # # Get kid scores
+    kid = DynamicKID(diffusion_type="ddim", img_size=64)
+
+    kid_score = 0
+    batch_counter = 0
+    for batch in train_dataset:
+        kid.update_state(batch, model)
+        kid_score += kid.result().numpy()
+        print(kid.result().numpy())
+        batch_counter += 1
+
+    kid_score /= batch_counter
+    print(kid_score)
+    # model.download_images(num_rows=3, num_cols=6)
