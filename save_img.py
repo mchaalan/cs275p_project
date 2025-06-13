a71372490bfd0a22d563a9e931be2a7d6896fb51
Mohamad Chaalan <mohamad@molecularinstruments.com>
2025-06-13 15:40:08 -0700

    uploaded figures and metadata

--- save_img.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras.preprocessing.image import save_img
-
-def save_image(image_tensor, filename):
-    # Convert the EagerTensor to a NumPy array
-    numpy_array = image_tensor.numpy()
-
-    # Save the NumPy array as a PNG image
-    save_img(filename, numpy_array)
